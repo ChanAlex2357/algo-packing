5b20b754f0b2ae84fd2b0a10a5b3eed71ecb5c3f
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-07-01 11:45:22 +0300

    init-repository

---
+++ Packing1D/__init__.py
@@ -0,0 +1,35 @@
+from bacs import Bac1D
+from objects import PackingObject1D
+from packingException import IncompatibleBacException
+def worstFit1D( B:int , objects:list):
+    '''
+        Insérer chaque objet dans le bac qui laissera le plus d’espace restant après
+        insertion
+
+        ARGS:
+            - B : la longueur des bacs sur un axe
+            - objects : la liste des objets a inserer correspondant a une taille si chacun
+    '''
+    # la liste des bacs
+    bacs = []
+    # Initialiser avec le bac numero 1
+    num_bac = 1
+    bacs.append(Bac1D(B,num_bac))
+    for packing_object in objects:
+        added = False
+        for bac in bacs:
+            try:
+                bac.add_object(packing_object)
+                added = True
+                break
+            except IncompatibleBacException :
+                added = False
+        if not added :
+            num_bac+=1
+            new_bac = Bac1D(B,num_bac)
+            try:
+                new_bac.add_object(packing_object)
+                bacs.append(new_bac)
+            except IncompatibleBacException :
+                # Exception si l'objet ne peut entrer dans aucune bac
+                pass
---
+++ main.py
@@ -0,0 +1,17 @@
+import Packing1D as p1d
+from objects import PackingObject1D
+
+objects = []
+
+objects.append( PackingObject1D(5) )
+objects.append( PackingObject1D(9) )
+objects.append( PackingObject1D(3) )
+objects.append( PackingObject1D(6) )
+objects.append( PackingObject1D(4) )
+objects.append( PackingObject1D(7) )
+objects.append( PackingObject1D(1) )
+
+p1d.worstFit1D(15,objects)
+
+for obj in objects:
+    print(obj)
