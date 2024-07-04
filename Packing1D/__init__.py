0f44060aee0c083bdda214f88489ca5b02630d7a
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-07-04 19:57:58 +0300

    worts fit method comment

--- Packing1D/__init__.py
+++ Packing1D/__init__.py
@@ -5,10 +5,13 @@ def worstFit1D( B:int , objects:list):
     '''
         Insérer chaque objet dans le bac qui laissera le plus d’espace restant après
         insertion
+        Les objets seront associes directement a un bac a par l'attribut num_bac , et ajouter au liste des objets de bac
 
         ARGS:
             - B : la longueur des bacs sur un axe
             - objects : la liste des objets a inserer correspondant a une taille si chacun
+        RETURN : 
+            - La liste des bacs utiliser pour faire le packing
     '''
     # la liste des bacs
     bacs = []
