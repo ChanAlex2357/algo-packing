b74787bbbc626b3f33c5c0ec5826ccad6fb96617
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-07-10 11:15:36 +0300

    description hfdh

--- Packing2D/__init__.py
+++ Packing2D/__init__.py
@@ -2,6 +2,8 @@ from bacs.Bac2D import Bac2D
 def next_fit_decreasing_height(width,height,objects:list):
     '''
         Fait le 2D packing des objets dans des boites de taille width x hight
+        Suivant l'algo Next Fit Decreasing Height (NFDH) qui consite a garder le
+        dernier bac et le remplir tant que les objets sont en decreasing hheight
         ARGS :
             - la longueur des bacs
             - la hauteur des bacs
@@ -11,5 +13,6 @@ def next_fit_decreasing_height(width,height,objects:list):
     '''
     bacs = []
     bac_num = 1
-    # bacs.append(Bac2D(width,height,num_bac))
+    bacs.append(Bac2D(width,height,bac_num))
+    # 
     
