89e4f5df4ecebfcaf6a0fc6e7e8a4a3cf7ed7456
sergianafr <sergiravelo@gmail.com>
2024-07-17 23:27:32 +0300

    brute force 2D rotation et algo tsotra

--- 2D-Rotation/__init__.py
+++ 2D-Rotation/__init__.py
@@ -4,7 +4,7 @@ from objects.Cercle import Cercle
 from objects.Triangle import Triangle
 from objects.Rectangle import Rectangle
 
-def place_objects_heuristically(objects, canvas_width, canvas_height):
+def brute_force(objects, canvas_width, canvas_height):
     placed_objects = []
     for obj in objects:
         placed = False
@@ -55,3 +55,59 @@ def place_objects_heuristically(objects, canvas_width, canvas_height):
         if not placed:
             print("Failed to place object:", obj)
     return placed_objects
+
+
+def placer_objet(objects, bac_width, bac_height):
+    placed_objects = []
+    last_x, last_y = 0, 0
+
+    for obj in objects:
+        placed = False
+
+        if isinstance(obj, Rectangle):
+            for rotation in [(obj.width, obj.height), (obj.height, obj.width)]:
+                obj.width, obj.height = rotation
+                if obj.can_be_placed(placed_objects, last_x, last_y, bac_width, bac_height):
+                    placed_objects.append((last_x, last_y, Rectangle(obj.width, obj.height)))
+                    last_x += obj.width
+
+                    # check si il faut changer d'etage
+                    if last_x >= bac_width:
+                        last_x = 0
+                        last_y += obj.height
+                    placed = True
+                    break
+
+        elif isinstance(obj, Triangle):
+            for rotation in [(obj.base, obj.height), (obj.height, obj.base)]:
+                obj.base, obj.height = rotation
+                if obj.can_be_placed(placed_objects, last_x, last_y, bac_width, bac_height):
+                    placed_objects.append((last_x, last_y, Triangle(obj.base, obj.height)))
+                    last_x += obj.base
+
+                    if last_x >= bac_width:
+                        last_x = 0
+                        last_y += obj.height
+                    placed = True
+                    break
+        else:
+            
+            if obj.can_be_placed(placed_objects, last_x, last_y, bac_width, bac_height):
+                placed_objects.append((last_x, last_y, obj))
+                last_x += obj.radius*2
+                if last_x >= bac_width:
+                    last_x = 0
+                    last_y += obj.radius*2
+                placed = True
+                break
+        
+
+        if not placed:
+            print("Failed to place object:", obj)
+            last_x += obj.width
+            if last_x >= bac_width:
+                last_x = 0
+                last_y += obj.height
+
+    return placed_objects
+
